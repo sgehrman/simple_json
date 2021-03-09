7a4432a7bca5c1a009d723e006971751858e4a78
Steve Gehrman <sgehrman@gmail.com>
2021-03-09 02:42:13 -0800

    update

--- example/lib/external_account_alias.dart
+++
@@ -1,9 +0,0 @@
-import 'package:simple_json_mapper/simple_json_mapper.dart';
-
-import 'account.dart';
-
-class Alias {}
-
-// This can be useful for when the types are defined in a package outside of your code.
-@JObj()
-class ExternalAccountAlias = Account with Alias;
